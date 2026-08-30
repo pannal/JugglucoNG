5e19433890466489b64235f94b94398857d0f7a5
pannal <panniiii@gmail.com>
2026-08-31 00:29:49 +0100

    Avoid Clone teardown deadlocks
    Never call libjuice while holding the Clone acknowledgement mutex, and protect shared acknowledgement state consistently. Close receiver gates synchronously, then run host quiescence and ICE destruction on a dedicated worker so disabling Clone cannot block Android input dispatch.

--- Common/src/main/java/tk/glucodata/Natives.java
+++ Common/src/main/java/tk/glucodata/Natives.java
@@ -1221,6 +1221,8 @@ public class Natives {
 
         public static native boolean getHostDeactivated(int pos);
 
+        public static native void prepareHostDeactivation(int pos);
+
         public static native void setHostDeactivated(int pos, boolean val);
 
         // public static native long makeSIdataptr(String gegs);
